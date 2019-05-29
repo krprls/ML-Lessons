8f4aa76e8ebcbf977fe99a30a8eafd065677872d
AB <aarushban@gmail.com>
2019-05-29 15:07:51 -0700

    added quotes around text

--- Guess_My_Mood/data_generation_mood.py
+++ Guess_My_Mood/data_generation_mood.py
@@ -1,4 +1,5 @@
-import pandas as pd;
+import pandas as pd
+import csv
 
 
 #extract only happiness and sadness columns
@@ -19,16 +20,31 @@ def generate_data(total_samples=2001, file_name = "medium_data.csv"):
     data = pd.read_csv("data2/mood_data.csv")
     data = data.sample(n=total_samples, replace=False).reset_index(drop=True)
     data.to_csv('data2/' + file_name, index = None, header=True) #Don't forget to add '.csv' at the end of the path
+
+
+
+def filter():
+    data = pd.read_csv("data2/guess_mood_1.csv")
     print(data)
+    # for index in range(0,len(data)):
+    #     data['content'][index] = f'"{data['content'][index]}"'
+    # data.to_csv('data2/guess_mood_0.csv', index = None, header=True) #Don't forget to add '.csv' at the end of the path
+    # data.update(data[['content']].astype(str))
+    # for index in range(0,len(data)):
+    #      data['content'][index] = '\"' + data['content'][index] + '\"'
+    print(data)
+    data.to_csv('data2/guess_mood_0.csv', index = None, header=True, quoting=csv.QUOTE_NONNUMERIC)
+
     
 
 
 if __name__ == "__main__":
-    generate_data(100, "guess_mood_1.csv") #lesson 1
-    generate_data(500, "guess_mood_2.csv") #lesson 2
-    generate_data(2000, "guess_mood_3.csv") #lesson 3
-    generate_data(5000, "guess_mood_4.csv") #lesson 4
-    generate_data(10000, "guess_mood_5.csv") #lesson 5(?)
+    # generate_data(500, "guess_mood_2.csv") #lesson 2
+    # generate_data(2000, "guess_mood_3.csv") #lesson 3
+    # generate_data(5000, "guess_mood_4.csv") #lesson 4
+    # generate_data(10000, "guess_mood_5.csv") #lesson 5(?)
+
+    filter()
     
 
 
