0c683cefec3aef514fb53bb74e102a291053ad59
AB <aarushban@gmail.com>
2019-05-29 14:18:28 -0700

    updated scratch project

--- Guess_My_Mood/data_generation_mood.py
+++ Guess_My_Mood/data_generation_mood.py
@@ -18,12 +18,17 @@ def extract_columns(dataset="data2/text_emotion.csv"):
 def generate_data(total_samples=2001, file_name = "medium_data.csv"):
     data = pd.read_csv("data2/mood_data.csv")
     data = data.sample(n=total_samples, replace=False).reset_index(drop=True)
+    data.to_csv('data2/' + file_name, index = None, header=True) #Don't forget to add '.csv' at the end of the path
     print(data)
     
 
 
 if __name__ == "__main__":
-    generate_data()
+    generate_data(100, "guess_mood_1.csv") #lesson 1
+    generate_data(500, "guess_mood_2.csv") #lesson 2
+    generate_data(2000, "guess_mood_3.csv") #lesson 3
+    generate_data(5000, "guess_mood_4.csv") #lesson 4
+    generate_data(10000, "guess_mood_5.csv") #lesson 5(?)
     
 
 
