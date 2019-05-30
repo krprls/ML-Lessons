927bec4ded56693745e085c0a7250198d6357cc5
AB <aarushban@gmail.com>
2019-05-30 16:17:29 -0700

    completed mood lesson 1

--- Guess_My_Mood/data_generation_mood.py
+++ Guess_My_Mood/data_generation_mood.py
@@ -7,8 +7,6 @@ def addQuotes(fileName="Guess_My_Mood_Dataset.csv"):
     train_data.to_csv('data2/' + fileName,index=False, quoting=csv.QUOTE_NONE,escapechar='\\')
     print("quotes added")
 
-
-
 #generate datasets
 #number of lines, file to save, file to read from
 def generate_data(total_samples=2001, file_name="guess_mood_0.csv", fileRead = "Guess_My_Mood_Dataset.csv"):
