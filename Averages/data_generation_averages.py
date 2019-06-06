1362753c73c3e166651622dfe0215748b0209ea1
AB <aarushban@gmail.com>
2019-06-06 13:29:29 -0700

    renamed files to be easier to locate

---
+++ Averages/data_generation_averages.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+
+def corrupt_dataset(file_read="",file_save="",fraction=0.1):
+    data = pd.read_csv("data/" +file_read)
+    #make fraction subset of data 
+    random_rows = data.sample(frac=fraction)
+
+    #drop the rows from the actual data
+    data = data.drop(random_rows.index)
+    
+    #flip the labels for this subset
+    random_rows.loc[random_rows['who_am_I'] == 'adult', 'who_am_I'] = 'a'
+    random_rows.loc[random_rows['who_am_I'] == 'child', 'who_am_I'] = 'adult'
+    random_rows.loc[random_rows['who_am_I'] == 'a', 'who_am_I'] = 'child'
+
+    #add the random rows back
+    data = data.append(random_rows)  
+
+def generate_average_data(total_samples=50, file_name = "averages_lesson_1_and_2.csv"):
+
+    #sample number should be greater than 10
+    if total_samples <= 10:
+        print("Please choose a number of samples greater than 10")
+        return
+
+    #generate the 4 columns first of random integers
+    A = np.array([np.random.randint(0, 1000) for i in range(0, total_samples)]).reshape(-1,1)
+    B = np.array([np.random.randint(0, 1000) for i in range(0, total_samples)]).reshape(-1,1)
+    C = np.array([np.random.randint(0, 1000) for i in range(0, total_samples)]).reshape(-1,1)
+    D = np.array([np.random.randint(0, 1000) for i in range(0, total_samples)]).reshape(-1,1)
+    
+    dataset = np.concatenate((A, B, C, D),axis=1)
+    average = np.mean(dataset, axis=1).reshape(-1,1)
+    # print(average)
+    average = np.round(average,2)
+    dataset = np.concatenate((A, B, C, D, average),axis=1)
+    np.random.shuffle(dataset)
+ 
+
+    data_header = 'A,B,C,D,AVERAGE'
+    np.savetxt('data/' + file_name ,dataset,header=data_header,  fmt='%s', delimiter=',', comments='')
+    print(file_name + " data saved")
+
+if __name__ == "__main__":
+
+    #LESSON 1 and 2 DATASETS
+    generate_average_data(50) #50 in total
+
+
+    #LESSON 3 DATASET (bigger)
+    generate_average_data(10000,"averages_lesson_3.csv")
+
+
+    #LESSON 4 DATASETS
+
+
+
+    # generate_score_first_data(3400, "age_lesson_3_full.csv") #3400 in total 
+    # extract_fraction() #only one adult in dataset
+    # extract_fraction(file_save="age_lesson_3_10_percent_adults.csv",number=170)
+    # extract_fraction(file_save="age_lesson_3_30_percent_adults.csv",number=510)
+    # extract_fraction(file_save="age_lesson_3_50_percent_adults.csv",number=850)
+    # extract_fraction(file_save="age_lesson_3_75_percent_adults.csv",number=1275)
+
+    # #LESSON 4 DATASET
+    # flip_data(file_save="age_lesson_4_corrupted_10_percent.csv", fraction=0.1)
+    # flip_data(file_save="age_lesson_4_corrupted_50_percent.csv", fraction=0.5)
+    # flip_data() #100% of data flipped
+
+
--- Guess_My_Mood/data_generation_mood.py
+++ Guess_My_Mood/data_generation_mood.py
@@ -6,20 +6,20 @@ import numpy as np
 
 #remove some number of happiness labels
 def skew_data(fraction=0.01, file_name="mood_4_one_sad.csv", fileRead="Guess_My_Mood_Dataset.csv"):
-    data = pd.read_csv("data2/" + fileRead)
+    data = pd.read_csv("data/" + fileRead)
     sad_length = len(data.query('feeling == "s"'))
     data = data.drop(data.query('feeling == "s"').sample(n=(sad_length - int(fraction*sad_length))).index)
     #np.random.shuffle(data)
-    data.to_csv("data2/" + file_name, index=False)
+    data.to_csv("data/" + file_name, index=False)
     # np.savetxt('data1/' + file_name ,data,header=data_header, fmt='%s', delimiter=',', comments='')
     print(file_name+ " data saved")
 
 
 # number of lines, file to save, file to read from
 def generate_data(total_samples=2001, file_name="guess_mood_0.csv", fileRead="Guess_My_Mood_Dataset.csv"):
-    data = pd.read_csv("data2/" + fileRead)
+    data = pd.read_csv("data/" + fileRead)
     data = data.sample(n=total_samples, replace=False).reset_index(drop=True)
-    data.to_csv("data2/" + file_name, index=False) #Don't forget to add '.csv' at the end of the path
+    data.to_csv("data/" + file_name, index=False) #Don't forget to add '.csv' at the end of the path
 
 if __name__ == "__main__": 
     generate_data(total_samples=50, file_name="mood_lesson_1_and_2.csv") #LESSSON 1 AND 2
