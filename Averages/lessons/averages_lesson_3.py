59bd9e49aeb4b1849c1d403f03d92ac5405712f0
AB <aarushban@gmail.com>
2019-06-20 11:41:43 -0700

    Averages: checking new float too

--- Averages/lessons/averages_lesson_3.py
+++ Averages/lessons/averages_lesson_3.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
  
 
         tries += 1
-        if float(old_ml_prediction):
+        if float(old_ml_prediction) and float(new_ml_prediction):
             trial_error_old = abs(old_ml_prediction - ave_via_formula)
             average_error_old = (average_error_old + trial_error_old) / tries
 
