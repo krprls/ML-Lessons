a4850f0187a847cb29ea72a6c412ad81826994a4
AB <aarushban@gmail.com>
2019-06-20 15:18:27 -0700

    All: Removed old files

--- Guess_My_Age/data_generation_age.py
+++ Guess_My_Age/data_generation_age.py
@@ -59,7 +59,7 @@ def generate_score_first_data(total_samples=2001, file_name = "medium_data.csv")
 
     #height follows a gaussian distribution
     height = np.array([np.random.normal(5.5,0.3) if i==1 else np.random.uniform(2,6) for i in score]).reshape(-1,1)
-    num_countries = np.array([np.random.randint(0,10) if i==0 else np.random.randint(0,100) for i in score]).reshape(-1,1)
+    num_countries = np.array([np.random.randint(0,i+5) if i < 3 else np.random.randint(0,10) for i in height]).reshape(-1,1)
     years_school = np.array([np.random.randint(0,i) if i <= 4 else np.random.randint(0,20) for i in height]).reshape(-1,1)
 
     #reshaping arrays 
@@ -89,37 +89,6 @@ def generate_score_first_data(total_samples=2001, file_name = "medium_data.csv")
     # print(dataset)
 
 
-    
-
-#ORIGINAL ALGORITHM
-def generate_data(num_samples_1=1400, num_samples_2=600, file_name = "large_sample.csv"):
-    # Generate samples for the guess my age dem
-    # We generate two sets of samples based on the age, there are 1400 samples 
-    # for height 2-5 and 600 for height 5-6.
-    height = np.concatenate((np.random.uniform(2,5,(num_samples_1,1)), np.random.uniform(5,6,(num_samples_2,1))))
-    # For ages 2-5, features such as years in school cannot be greater than 5, randomness is limited to this constraint
-    # Similarly, randomess for countries is limited based on the constraint that soemone with height less than 3 might not
-    # have travelled more than 4 countries
-    years_school = np.array([np.random.randint(0,i) if i<3.5 else np.random.randint(0,15) for i in height]).reshape(-1,1)
-    num_countries = np.array([np.random.randint(0,4) if i<=3 else np.random.randint(0,20) for i in height ]).reshape(-1,1)
-    years_school.shape
-    num_countries.shape
-
-    # Adult is a person above 16 years
-    score = np.array([np.random.randint(0,i+5) for i in years_school]).reshape(-1,1)
-    dataset = np.concatenate((num_countries, years_school, height, score),axis=1)
-
-    score[score<=4] = 0
-    score[height<4.5] = 0
-    score[years_school>=11] = 1
-
-
-    print("score is " + str(np.sum(score)))
-    dataset = np.concatenate((num_countries, years_school, height, score),axis=1)
-    np.random.shuffle(dataset)
-    data_header = 'num_countries,years_school,height,adult'
-    np.savetxt('data/' + file_name ,dataset,header=data_header,fmt='%.2f',delimiter=',', comments='')
-    print(file_name + " data saved")
 
 
 if __name__ == "__main__":
