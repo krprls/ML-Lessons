3c6e2c876af04a07a622b08939b6b6f8c8533000
AB <aarushban@gmail.com>
2019-06-20 11:32:12 -0700

    Averages: Added space

--- Averages/lessons/averages_lesson_3.py
+++ Averages/lessons/averages_lesson_3.py
@@ -102,7 +102,7 @@ if __name__ == "__main__":
         tries += 1
         if float(old_ml_prediction):
             trial_error_old = abs(old_ml_prediction - ave_via_formula)
-            average_error_old= (average_error_old + trial_error_old) / tries
+            average_error_old = (average_error_old + trial_error_old) / tries
 
             trial_error_new = abs(new_ml_prediction - ave_via_formula)
             average_error_new  = (average_error_new + trial_error_new) / tries
