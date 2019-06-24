3d7fc7aa88322ef322e8791e9d4ddc842fe07d3e
AB <aarushban@gmail.com>
2019-06-24 16:38:32 -0700

    Averages: Stripped URL of whitespace

--- Averages/projects/averages_project_1.py
+++ Averages/projects/averages_project_1.py
@@ -59,6 +59,7 @@ if __name__ == "__main__":
     while base_url not in url:
         print("Please make sure your endpoint URL starts with " + base_url)
         url = get_validated_input("What is your endpoint URL?\n", 'string')
+    url = url.strip()
     
     while play.lower() == "y":
         num1 = get_validated_input("Please enter your first number: ", 'float')
