328877f674dcfd6a1a1195070a72041f0c74e862
AB <aarushban@gmail.com>
2019-07-01 16:09:46 -0700

    Sorting_Hat 1_3_4: Moved Main function to the top for sorting hat (

--- Sorting_Hat/projects/sorting_hat_project_1_3.py
+++ Sorting_Hat/projects/sorting_hat_project_1_3.py
@@ -2,7 +2,17 @@ import requests
 import json as json
 import numpy as np
 
+def main():
 
+    print("Today we will use ML to sort you into a Harry Potter wizard house!")
+ 
+    url = input("What is your endpoint URL?\n")
+    url = url.strip()
+
+    trait = input("Tell me something about yourself!\n")
+
+    data = {"description": trait}
+    get_prediction(url, data)
 
 #calculate and print out the prediction based on ML 
 def get_prediction(url, data={"description:", "I love to help others!"}):
@@ -10,68 +20,20 @@ def get_prediction(url, data={"description:", "I love to help others!"}):
     response = getattr(r,'_content').decode("utf-8")
     response = json.loads(response)
     prediction_object = json.loads(response['body'])
-
-    if 'Message' in prediction_object and "dummy response" in prediction_object['Message']:
-        print("Please train your model to get better predictions.")
-    
+    label = ""    
     if 'predicted_label' in prediction_object:
         label = prediction_object['predicted_label']
 
-    print("ML Prediction: ", label)
+    print("ML prediction") 
+    print("\tLabel: ", label)
+    print("\tModel: ", prediction_object['Model'])
+    print("\tMessage: ", prediction_object['Message'])
     return label
 
-def get_validated_input(question,input_type):
-
-    variable = input(question)
-
-    while True:
-        if input_type == 'float':
-            try:
-                user_input = float(variable)
-            except ValueError:
-                variable = input("You must enter a float (e.g.: 1.3).\n" + question)
-        elif input_type == 'integer':
-            try:
-                user_input = int(variable)
-            except ValueError:
-                variable = input("You must enter an integer (e.g.: 1).\n" + question)
-        elif input_type == 'string':
-             try:
-                user_input = str(variable)
-             except ValueError:
-                variable = input("You must enter a string.\n" + question)
-        break
-    return variable
-
 if __name__ == "__main__":
+    main()
 
-    correct_tries = 0
-    tries = 0
-    play = "y"
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
 
-    print("Today we will use ML to sort you into a Harry Potter wizard house!")
- 
-    url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
-    url = url.strip()
 
-    while play.lower() == "y":
-        trait = get_validated_input("Tell me something about yourself!\n",'string')
-
-        data = {"description": trait}
-        ml_prediction = get_prediction(url, data)
-
-        if ml_prediction != "Unable to predict":
-            tries += 1
-            correct = input("Was the model's prediction correct? (y/n)\n")
-            if correct.lower() == "y":
-                correct_tries += 1 
-
-        print("Correct Tries: ", correct_tries,  " out of ", tries)
-
-        play = input("Want to try again? (y/n)\n")
 
     
