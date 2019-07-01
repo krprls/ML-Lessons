9ab87212a65ed23a0a1638270d8096e0d61e8094
AB <aarushban@gmail.com>
2019-07-01 16:20:50 -0700

    Averages_1: Moved Main function to the top for averages ( fixes #127 )

--- Averages/projects/averages_project_1.py
+++ Averages/projects/averages_project_1.py
@@ -2,7 +2,21 @@ import requests
 import json as json
 import numpy as np
 
+def main():
+    print("Hello! Today we are going to try to compute the average of four numbers with a Machine Learning model!")
 
+    url = input("What is your endpoint URL?\n")
+    url = url.strip()
+
+    num1 = input("Please enter your first number: ")
+    num2 = input("Please enter your second number: ")
+    num3 = input("Please enter your third number: ")
+    num4 = input("Please enter your fourth number: ")
+
+    #pass in the data
+    data = {"A": num1, "B": num2,"C": num3, "D": num4}
+
+    get_prediction(url, data)
 
 #calculate and print out the prediction
 def get_prediction(url, data={"A":48,"B":23,"C":38,"D":54}):
@@ -10,77 +24,20 @@ def get_prediction(url, data={"A":48,"B":23,"C":38,"D":54}):
     response = getattr(r,'_content').decode("utf-8")
     response = json.loads(response)
     prediction_object = json.loads(response['body'])
-
-    if 'Message' in prediction_object and "dummy response" in prediction_object['Message']:
-        print("Please train your model to get better predictions.")
-    
+    label = ""    
     if 'predicted_label' in prediction_object:
         label = prediction_object['predicted_label']
-    else:
-        label = "This model is unable to predict at this point."
 
-    print("ML prediction: ", label)
+    print("ML prediction") 
+    print("\tLabel: ", label)
+    print("\tModel: ", prediction_object['Model'])
+    print("\tMessage: ", prediction_object['Message'])
     return label
 
 
-def get_validated_input(question,input_type):
-
-    variable = input(question)
-
-    while True:
-        if input_type == 'float':
-            try:
-                user_input = float(variable)
-            except ValueError:
-                variable = input("You must enter a float (e.g.: 1.3).\n" + question)
-        elif input_type == 'integer':
-            try:
-                user_input = int(variable)
-            except ValueError:
-                variable = input("You must enter an integer (e.g.: 1).\n" + question)
-        elif input_type == 'string':
-             try:
-                user_input = str(variable)
-             except ValueError:
-                variable = input("You must enter a string.\n" + question)
-        break
-    return variable
-
 if __name__ == "__main__":
+    main()
 
-    #getting player input 
-    correct_tries = tries = 0
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
-    play = "y"
-
-    print("Hello! Today we are going to try to compute the average of four numbers with a Machine Learning model!")
-
-    url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
-    url = url.strip()
-    
-    while play.lower() == "y":
-        num1 = get_validated_input("Please enter your first number: ", 'float')
-        num2 = get_validated_input("Please enter your second number: ", 'float')
-        num3 = get_validated_input("Please enter your third number: ", 'float')
-        num4 = get_validated_input("Please enter your fourth number: ", 'float')
-
-        #pass in the data
-        data = {"A": num1, "B": num2,"C": num3, "D": num4}
- 
-        get_prediction(url, data)
-
-        tries += 1
-        correct_response = input("Was the ML prediction correct? (y/n)\n")
-
-        if correct_response.lower() == "y":
-            correct_tries += 1
-        
-        print("Correct Tries: ", correct_tries,  " out of ", tries)
-        
-        play = input("Want to try again? (y/n)\n")
 
 
 
