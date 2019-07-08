6d5c7d53cfdc09ae1a9028ef57f6c2c3b61749c0
AB <aarushban@gmail.com>
2019-07-08 10:08:03 -0700

    Age_Project_2: Removed extra spaces and validation code. Moved rules prediction label on a separate tabbed line to be consistent with the ml prediction format.

--- Guess_My_Age/projects/age_project_2.py
+++ Guess_My_Age/projects/age_project_2.py
@@ -2,32 +2,23 @@ import requests
 import json as json
 import numpy as np
 
-
-
 def main():
 
     correct_ml_tries = 0
     correct_rules_tries = 0
     total_tries = 0
 
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
-
-    play = "y"
     print("Hello! Today we are going to use ML to guess whether you are a child or an adult!")
 
     url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
-    url = url.strip()
     
-    while play.lower() == "y":
+    play = "y"
+    while play == "y":
+        visited_countries = input("How many countries have you visited?\n")
+        years_in_school = input("How many years did you spend in school?\n")
+        height = input("What is your height?\n")
 
-        visited_countries = get_validated_input("How many countries have you visited?\n",'integer')
-        years_in_school = get_validated_input("How many years did you spend in school?\n",'integer')
-        height = get_validated_input("What is your height?\n",'float')
 
-    
         data = {"num_countries":visited_countries, "years_school":years_in_school, "height":height}
 
         ml_prediction = get_prediction(url, data)
@@ -40,13 +31,13 @@ def main():
             correct_rules_tries += 1
             if ml_prediction == rules_prediction:
                 correct_ml_tries += 1
-        elif ml_prediction != rules_prediction and ml_prediction != "This model is unable to predict at this point.":
+        elif ml_prediction != rules_prediction and ml_prediction != "Unable to predict.":
             correct_ml_tries += 1
         
         print("Correct ML Tries: ", correct_ml_tries, " out of ", total_tries)
         print("Correct Rules Tries: ", correct_rules_tries, " out of ", total_tries)
 
-        play = input("Want to try again? (y/n)\n")
+        print("Press Ctrl + C to stop at anytime. Moving on to the next round.")
             
 
 #calculate and print out the prediction based on ML 
@@ -55,54 +46,27 @@ def get_prediction(url, data={"num_countries":48, "years_school":2, "height":5.1
     response = getattr(r,'_content').decode("utf-8")
     response = json.loads(response)
     prediction_object = json.loads(response['body'])
-
-    if 'Message' in prediction_object and "dummy response" in prediction_object['Message']:
-        print("Please train your model to get better predictions.")
-    
+    label = ""    
     if 'predicted_label' in prediction_object:
         label = prediction_object['predicted_label']
-    else:
-        label = "This model is unable to predict at this point."
 
-    print("ML prediction: ", label)
+    print("ML prediction") 
+    print("\tLabel: ", label)
+    print("\tModel: ", prediction_object['Model'])
+    print("\tMessage: ", prediction_object['Message'])
     return label
 
 
 #calculate and print out the prediction based on CONDITIONS
 def get_conditional_prediction(countries, years, height):
-    prediction = "child"
+    label = "child"
 
     if countries > 10 and years > 10 and height > 3:
-        prediction = "adult"
+        label = "adult"
 
-
-    print("Rules prediction: ", prediction)
-    return prediction
-
-def get_validated_input(question, input_type):
-
-    variable = input(question)
-
-    while True:
-        if input_type == 'float':
-            try:
-                user_input = float(variable)
-                break
-            except ValueError:
-                variable = input("You must enter a float (e.g.: 1.3).\n" + question)
-        elif input_type == 'integer':
-            try:
-                user_input = int(variable)
-                break
-            except ValueError:
-                variable = input("You must enter an integer (e.g.: 1).\n" + question)
-        elif input_type == 'string':
-             try:
-                user_input = str(variable)
-                break
-             except ValueError:
-                variable = input("You must enter a string.\n" + question)
-    return variable
+    print("Rules prediction")
+    print("\tLabel: ", label)
+    return label
 
 if __name__ == "__main__":
     main()
