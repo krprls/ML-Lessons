04a808f87d5175e8ccbcc8485820ca3ac52956c7
AB <aarushban@gmail.com>
2019-07-08 10:23:54 -0700

    Mood_Project_2: Moved main function to the top

--- Guess_My_Mood/projects/mood_project_2.py
+++ Guess_My_Mood/projects/mood_project_2.py
@@ -3,7 +3,48 @@ import json as json
 import numpy as np
 
 
+def main():
+    correct_ml_tries = 0
+    correct_rules_tries = 0
+    total_tries = 0
 
+    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
+
+    play = "y"
+    print("Hello! Today we will use machine learning to guess how you are feeling!")
+
+    url = input("What is your endpoint URL?\n")
+    while base_url not in url:
+        print("Please make sure your endpoint URL starts with " + base_url)
+        url = get_validated_input("What is your endpoint URL?\n", 'string')
+    url = url.strip()
+    
+    while play.lower() == "y":
+
+        mood = get_validated_input("What's on your mind?\n", 'string')
+
+        data = {"sentence": mood}
+
+        ml_prediction = get_prediction(url, data)
+        rules_prediction = get_conditional_prediction(mood)
+
+
+        total_tries += 1
+        user_validation = input("Was the rules prediction \"" + rules_prediction + "\" correct? (y/n)\n")
+
+        if user_validation.lower() == "y":
+            correct_rules_tries += 1
+            if ml_prediction == rules_prediction:
+                correct_ml_tries += 1
+        elif ml_prediction != rules_prediction and ml_prediction != "Unable to predict":
+                correct_ml_tries += 1
+            
+        print("Correct ML Tries: ", correct_ml_tries, " out of ", total_tries)
+        print("Correct Rules Tries: ", correct_rules_tries, " out of ", total_tries)
+
+        play = input("Want to try again? (y/n)\n")
+
+            
 #calculate and print out the prediction based on ML 
 def get_prediction(url, data={"sentence:", "I love to help others!"}):
     r = requests.post(url, data=json.dumps(data))
@@ -54,45 +95,6 @@ def get_validated_input(question, input_type):
     return variable
 
 if __name__ == "__main__":
-
-    correct_ml_tries = 0
-    correct_rules_tries = 0
-    total_tries = 0
-
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
-
-    play = "y"
-    print("Hello! Today we will use machine learning to guess how you are feeling!")
-
-    url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
-    url = url.strip()
-    
-    while play.lower() == "y":
-
-        mood = get_validated_input("What's on your mind?\n", 'string')
-
-        data = {"sentence": mood}
-
-        ml_prediction = get_prediction(url, data)
-        rules_prediction = get_conditional_prediction(mood)
-
-
-        total_tries += 1
-        user_validation = input("Was the rules prediction \"" + rules_prediction + "\" correct? (y/n)\n")
-
-        if user_validation.lower() == "y":
-            correct_rules_tries += 1
-            if ml_prediction == rules_prediction:
-                correct_ml_tries += 1
-        elif ml_prediction != rules_prediction and ml_prediction != "Unable to predict":
-                correct_ml_tries += 1
-            
-        print("Correct ML Tries: ", correct_ml_tries, " out of ", total_tries)
-        print("Correct Rules Tries: ", correct_rules_tries, " out of ", total_tries)
-
-        play = input("Want to try again? (y/n)\n")
+    main()
 
     
