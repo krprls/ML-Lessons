1609986bc6ce1ad0a5bfb9713f76d67dd10a29ad
AB <aarushban@gmail.com>
2019-07-08 11:38:06 -0700

    Averages_2_4: Made consistent with all other project 2s, message key needs to exist in regression models

--- Averages/projects/averages_project_2_4.py
+++ Averages/projects/averages_project_2_4.py
@@ -3,85 +3,22 @@ import json as json
 import numpy as np
 
 
-#calculate and print out the prediction
-def get_prediction(url, data={"A":48,"B":23,"C":38,"D":54}):
-    r = requests.post(url, data=json.dumps(data))
-    response = getattr(r,'_content').decode("utf-8")
-    response = json.loads(response)
-    prediction_object = json.loads(response['body'])
-
-    if 'Message' in prediction_object and "dummy response" in prediction_object['Message']:
-        print("Please train your model to get better predictions.")
-    
-    if 'predicted_label' in prediction_object:
-        label = prediction_object['predicted_label']
-    else:
-        label = "This model is unable to predict at this point."
-
-    print("ML prediction: ", label)
-    return label
-
-#calculate and print out the prediction based on FORMULA
-def formula(num1=50,num2=70,num3=80,num4=90):
-
-    average = ""
-    num1 = float(num1)
-    num2 = float(num2)
-    num3 = float(num3)
-    num4 = float(num4)
-    average = (num1 + num2 + num3 + num4) / 4
-
-    print("Average via Formula: ", average)
-
-    return average
-
-def get_validated_input(question,input_type):
-
-    variable = input(question)
-
-    while True:
-        if input_type == 'float':
-            try:
-                user_input = float(variable)
-            except ValueError:
-                variable = input("You must enter a float (e.g.: 1.3).\n" + question)
-        elif input_type == 'integer':
-            try:
-                user_input = int(variable)
-            except ValueError:
-                variable = input("You must enter an integer (e.g.: 1).\n" + question)
-        elif input_type == 'string':
-             try:
-                user_input = str(variable)
-             except ValueError:
-                variable = input("You must enter a string.\n" + question)
-        break
-    return variable
-
-
-if __name__ == "__main__":
-
-
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
+def main():
     tries = 0
     trial_error = 0
     average_error = 0
 
     print("Hello! Today we are going to try to compute the average of four numbers with a Machine Learning model!")
 
-
     url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
     url = url.strip()
 
     play = "y"
     while play.lower() == "y":
-        num1 = get_validated_input("Please enter your first number: ", 'float')
-        num2 = get_validated_input("Please enter your second number: ", 'float')
-        num3 = get_validated_input("Please enter your third number: ", 'float')
-        num4 = get_validated_input("Please enter your fourth number: ", 'float')
+        num1 = input("Please enter your first number: ")
+        num2 = input("Please enter your second number: ")
+        num3 = input("Please enter your third number: ")
+        num4 = input("Please enter your fourth number: ")
 
         #pass in the data
         data = {"A": num1, "B": num2,"C": num3, "D": num4}
@@ -98,9 +35,36 @@ if __name__ == "__main__":
         print("Average Error: ", average_error)
         print("Total trials: ", tries)
   
-        
-        play = input("Want to play again? (y/n)\n")
+        print("Press Ctrl + C to stop at anytime. Moving on to the next round.")
 
+#calculate and print out the prediction
+def get_prediction(url, data={"A":48,"B":23,"C":38,"D":54}):
+    r = requests.post(url, data=json.dumps(data))
+    response = getattr(r,'_content').decode("utf-8")
+    response = json.loads(response)
+    prediction_object = json.loads(response['body'])
+    label = ""    
+    if 'predicted_label' in prediction_object:
+        label = prediction_object['predicted_label']
+
+    print("ML prediction") 
+    print("\tLabel: ", label)
+    print("\tModel: ", prediction_object['Model'])
+    # print("\tMessage: ", prediction_object['Message']) #UNCOMMENT WHEN MESSAGE KEY EXISTS FOR REGRESSION MODELS
+    return label
+
+#calculate and print out the prediction based on FORMULA
+def formula(num1=50,num2=70,num3=80,num4=90):
+
+    label = (float(num1) + float(num2) + float(num3) + float(num4)) / 4
+
+    print("Average via Formula")
+    print("\tLabel: ", label)
+    return label
+
+
+if __name__ == "__main__":
+    main()
 
     
 
