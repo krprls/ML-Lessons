875ebaea5e0771d40446f824ab1d77ff2b4132c0
AB <aarushban@gmail.com>
2019-07-08 11:32:33 -0700

    Sorting_Hat_2: Moved main function to top, removed extra validation code and made prediction function consisten with other project 2s

--- Sorting_Hat/projects/sorting_hat_project_2.py
+++ Sorting_Hat/projects/sorting_hat_project_2.py
@@ -3,80 +3,19 @@ import json as json
 import numpy as np
 
 
-
-#calculate and print out the prediction based on ML 
-def get_prediction(url, data={"description:", "I love to help others!"}):
-    r = requests.post(url, data=json.dumps(data))
-    response = getattr(r,'_content').decode("utf-8")
-    response = json.loads(response)
-    prediction_object = json.loads(response['body'])
-
-    if 'Message' in prediction_object and "dummy response" in prediction_object['Message']:
-        print("Please train your model to get better predictions.")
-    
-    if 'predicted_label' in prediction_object:
-        label = prediction_object['predicted_label']
-
-    print("ML Prediction: ", label)
-    return label
-
-def get_rules_prediction(sentence="I am ambitious"):
-    if "ambitious" in sentence:
-        response = "Slytherin"
-    elif "friend" in sentence:
-        response = "Hufflepuff"
-    elif "creative" in sentence:
-        response = "Ravenclaw"
-    elif "courage" in sentence:
-        response = "Gryffindor"
-    else:
-        response = "unsure"
-    
-    print("Rules prediction: " + response)
-    return response
-
-def get_validated_input(question,input_type):
-
-    variable = input(question)
-
-    while True:
-        if input_type == 'float':
-            try:
-                user_input = float(variable)
-            except ValueError:
-                variable = input("You must enter a float (e.g.: 1.3).\n" + question)
-        elif input_type == 'integer':
-            try:
-                user_input = int(variable)
-            except ValueError:
-                variable = input("You must enter an integer (e.g.: 1).\n" + question)
-        elif input_type == 'string':
-             try:
-                user_input = str(variable)
-             except ValueError:
-                variable = input("You must enter a string.\n" + question)
-        break
-    return variable
-
-if __name__ == "__main__":
-
+def main():
     correct_rules = 0
     correct_ml = 0
-    correct_tries = 0
     tries = 0
-    play = "y"
-    base_url = "https://cqzuqwmdp1.execute-api.us-east-1.amazonaws.com/Predict/"
 
+    play = "y"
     print("Today we will use ML to sort you into a Harry Potter wizard house!")
  
     url = input("What is your endpoint URL?\n")
-    while base_url not in url:
-        print("Please make sure your endpoint URL starts with " + base_url)
-        url = get_validated_input("What is your endpoint URL?\n", 'string')
     url = url.strip()  
 
     while play.lower() == "y":
-        trait = get_validated_input("Tell me something about yourself!\n",'string')
+        trait = input("Tell me something about yourself!\n")
 
         data = {"description": trait}
         ml_prediction = get_prediction(url, data)
@@ -96,5 +35,42 @@ if __name__ == "__main__":
         
         print("Correct ML: ", correct_ml, " out of ", tries)
         print("Correct Rules: ", correct_rules, " out of ", tries)
-        play = input("Want to try again? (y/n)\n")
+        print("Press Ctrl + C to stop at anytime. Moving on to the next round.")
+
+#calculate and print out the prediction based on ML 
+def get_prediction(url, data={"description:", "I love to help others!"}):
+    r = requests.post(url, data=json.dumps(data))
+    response = getattr(r,'_content').decode("utf-8")
+    response = json.loads(response)
+    prediction_object = json.loads(response['body'])
+    label = ""    
+    if 'predicted_label' in prediction_object:
+        label = prediction_object['predicted_label']
+
+    print("ML prediction") 
+    print("\tLabel: ", label)
+    print("\tModel: ", prediction_object['Model'])
+    print("\tMessage: ", prediction_object['Message'])
+    return label
+
+def get_rules_prediction(sentence="I am ambitious"):
+    label = "Unable to predict"
+
+    if "ambitious" in sentence:
+        label = "Slytherin"
+    elif "friend" in sentence:
+        label = "Hufflepuff"
+    elif "creative" in sentence:
+        label = "Ravenclaw"
+    elif "courage" in sentence:
+        label = "Gryffindor"
+
+    print("Rules prediction")
+    print("\tLabel: ", label)
+    return label
+
+if __name__ == "__main__":
+    main()
+
+        
     
