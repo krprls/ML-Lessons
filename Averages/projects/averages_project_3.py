71def9a6924a3b57466a9adc1cf7391533c78e83
AB <aarushban@gmail.com>
2019-07-08 15:46:26 -0700

    Averages_3: removed flag and put bigger/smaller outside in main function

--- Averages/projects/averages_project_3.py
+++ Averages/projects/averages_project_3.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 #calculate and print out the prediction
-def get_prediction(url, flag, data={"A":48,"B":23,"C":38,"D":54}):
+def get_prediction(url, data={"A":48,"B":23,"C":38,"D":54}):
     r = requests.post(url, data=json.dumps(data))
     response = getattr(r,'_content').decode("utf-8")
     response = json.loads(response)
@@ -12,11 +12,6 @@ def get_prediction(url, flag, data={"A":48,"B":23,"C":38,"D":54}):
     label = ""    
     if 'predicted_label' in prediction_object:
         label = prediction_object['predicted_label']
-
-    if flag == 'smaller':
-        print("ML prediction (smaller dataset)")
-    else:
-        print("ML prediction (bigger dataset)")
 
     print("\tLabel: ", label)
     print("\tModel: ", prediction_object['Model'])
@@ -58,8 +53,11 @@ if __name__ == "__main__":
         #pass in the data
         data = {"A": num1, "B": num2,"C": num3, "D": num4}
 
-        old_ml_prediction = get_prediction(old_url, 'smaller', data) #from smaller dataset
-        new_ml_prediction = get_prediction(new_url, 'bigger', data) #from bigger dataset
+
+        print("ML prediction (smaller dataset)")
+        old_ml_prediction = get_prediction(old_url, data) #from smaller dataset
+        print("ML prediction (bigger dataset)")
+        new_ml_prediction = get_prediction(new_url, data) #from bigger dataset
         ave_via_formula = formula(num1, num2, num3, num4) #from formula
 
         tries += 1
