386bf3dddbe0ca3c8425f499651e84cae28ad7b2
AB <aarushban@gmail.com>
2019-07-10 12:55:11 -0700

    Zipped all age files, removed the corresponding single files from data/ directory

--- Guess_My_Age/data_generation_age.py
+++ Guess_My_Age/data_generation_age.py
@@ -3,6 +3,7 @@ import pandas as pd
 import glob
 import zlib
 import zipfile
+import os
 
 #project 4--FLIP DATA POINTS
 def flip_data(in_file="age_project_3_full.csv", out_file="age_project_4_corrupted_100_percent.csv", fraction=1):
@@ -92,28 +93,26 @@ def compress_files(regex="age_project*", out_file="age_project.zip"):
                out_file (str): The name of the zip file you want to generate.
           Returns: N/A
      """
-     # get all files you want to zip
-     files = glob.glob('data/' + regex)
+     files = glob.glob('data/' + regex)  # get all files you want to zip
 
-    # Select the compression mode ZIP_DEFLATED for compression
-    # or zipfile.ZIP_STORED to just store the file
+     # Select the compression mode ZIP_DEFLATED for compression
+     # or zipfile.ZIP_STORED to just store the file
      compression = zipfile.ZIP_DEFLATED
      # create the zip file first parameter path/name, second mode
-     zf = zipfile.ZipFile('data/' + out_file, mode="w")
+     zf = zipfile.ZipFile('data/' + out_file, mode="w") 
 
      try:
           for file_name in files:
                # Add file to the zip file
                # first parameter file to zip, second filename in zip
                zf.write(file_name, file_name, compress_type=compression)
+               os.remove(file_name) #remove file from data/ directory because already added to zipfile
      except FileNotFoundError:
           print("An error occurred")
      finally:
-          # Don't forget to close the file!
-          zf.close()
+          zf.close() # Don't forget to close the file!
 
-     #let user know file has been zipped
-     print(out_file + " has been zipped")
+     print(out_file + " has been zipped") # let user know file has been zipped
 
 if __name__ == "__main__":
 
