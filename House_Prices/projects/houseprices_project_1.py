9264029d84699bfce32279f0311b7f734bf3b739
AB <aarushban@gmail.com>
2019-07-10 09:45:44 -0700

    House_Price: Base code ready

--- House_Prices/projects/houseprices_project_1.py
+++ House_Prices/projects/houseprices_project_1.py
@@ -3,6 +3,27 @@ import json as json
 import numpy as np
 
 
+def main():
+    print("Hello! Today we are going to estimate the price of a house with a Machine Learning model!")
+
+    url = input("What is your endpoint URL?\n")
+    url = url.strip()
+    longitude = input("What is the longitude of this house?\n")
+    latitude = input("What is the latitude of this house?\n")
+    housing_median_age = input("What is the media age of houses in the area?\n")
+    total_rooms = input("What are the total number of rooms in the block?\n")
+    total_bedrooms = input("What are the total number of bedrooms in the block?\n")
+    population = input("What is the total population in this block?\n")
+    households = input("What is the number of households in this block?\n")
+    median_income = input("What is the median income in this block?\n")
+    median_house_value = input("What is the median household value in this block?\n")
+
+    #pass in the data
+    data = {"longitude": longitude, "latitude": latitude, "housing_median_age": housing_median_age,
+            "total_rooms": total_rooms, "total_bedrooms": total_bedrooms, "population": population,
+            "households": households, "median_income": median_income, "median_house_value": median_house_value}
+
+    get_prediction(url, data)
 
 #calculate and print out the prediction
 def get_prediction(url, data={"longitude":-122.5,"latitude":37.79,"housing_median_age":52,
@@ -19,3 +40,6 @@ def get_prediction(url, data={"longitude":-122.5,"latitude":37.79,"housing_media
     print("\tLabel: ", label)
     print("\tModel: ", prediction_object['Model'])
     return label
+
+if __name__ == "__main__":
+    main()
