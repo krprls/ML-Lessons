260093d0256e46da0c1c4b091d1a0c98b972d5b7
AB <aarushban@gmail.com>
2019-07-10 13:55:29 -0700

    Added zipping code

--- Averages/data_generation_averages.py
+++ Averages/data_generation_averages.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
+import glob
+import zlib
+import zipfile
+import os
 
 def corrupt_dataset(file_read="averages_project_3.csv",file_save="averages_project_4_10_percent_corrupt.csv",fraction=0.1):
     data = pd.read_csv("data/" + file_read)
@@ -44,6 +48,35 @@ def generate_average_data(total_samples=50, file_name = "averages_project_1_and_
     data_header = 'A,B,C,D,AVERAGE'
     np.savetxt('data/' + file_name ,dataset,header=data_header,  fmt='%s', delimiter=',', comments='')
     print(file_name + " data saved")
+
+def compress_files(regex="age_project*", out_file="age_project.zip"):
+     """
+          Zips files matching a regex
+          Args:
+               regex (str): pattern you want to match to filter out files
+               out_file (str): The name of the zip file you want to generate.
+          Returns: N/A
+     """
+     files = glob.glob('data/' + regex)  # get all files you want to zip
+
+     # Select the compression mode ZIP_DEFLATED for compression
+     # or zipfile.ZIP_STORED to just store the file
+     compression = zipfile.ZIP_DEFLATED
+     # create the zip file first parameter path/name, second mode
+     zf = zipfile.ZipFile('data/' + out_file, mode="w") 
+
+     try:
+          for file_name in files:
+               # Add file to the zip file
+               # first parameter file to zip, second filename in zip
+               zf.write(file_name, file_name, compress_type=compression)
+               os.remove(file_name) #remove file from data/ directory because already added to zipfile
+     except FileNotFoundError:
+          print("An error occurred")
+     finally:
+          zf.close() # Don't forget to close the file!
+
+     print(out_file + " has been zipped") # let user know file has been zipped
 
 if __name__ == "__main__":
 
