7ff9535a8ebd765396dd9639e4c293371f74f91c
AB <aarushban@gmail.com>
2019-07-11 11:09:37 -0700

    I added the bash command for updating mood files as well

--- update_all_files.sh
+++ update_all_files.sh
@@ -2,3 +2,5 @@
 python3 Guess_My_Age/data_generation_age.py Guess_My_Age/data/
 python3 utils/file_compression.py Guess_My_Age/data/ age_project_3* age_project_3.zip
 python3 utils/file_compression.py Guess_My_Age/data/ age_project_4* age_project_4.zip
+python3 Guess_My_Age/data_generation_age.py Guess_My_Mood/data/
+python3 utils/file_compression.py Guess_My_Mood/data/ mood_project_4* mood_project_4.zip
