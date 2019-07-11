519a4aa36914c753f0825598738c6562871a334f
AB <aarushban@gmail.com>
2019-07-11 11:12:48 -0700

    Added comments to update_mood.sh file

--- update_all_files.sh
+++ update_all_files.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+#Guess_My_Age data generation and compression
 python3 Guess_My_Age/data_generation_age.py Guess_My_Age/data/
 python3 utils/file_compression.py Guess_My_Age/data/ age_project_3* age_project_3.zip
 python3 utils/file_compression.py Guess_My_Age/data/ age_project_4* age_project_4.zip
-python3 Guess_My_Age/data_generation_age.py Guess_My_Mood/data/
+
+#Guess_My_Mood data generation and compression
+python3 Guess_My_Mood/data_generation_mood.py Guess_My_Mood/data/
 python3 utils/file_compression.py Guess_My_Mood/data/ mood_project_4* mood_project_4.zip
