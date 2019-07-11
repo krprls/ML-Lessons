9be9cc5c995baa743264d437501537f446a1de7f
AB <aarushban@gmail.com>
2019-07-11 14:29:22 -0700

    Added sorting_hat data_generation and zipping script to bash script

--- update_all_files.sh
+++ update_all_files.sh
@@ -12,3 +12,7 @@ python3 utils/file_compression.py Guess_My_Mood/data/ mood_project_4* mood_proje
 #Averages data generation and compression
 python3 Averages/data_generation_averages.py Averages/data/
 python3 utils/file_compression.py Averages/data/ averages_project_4* averages_project_4.zip
+
+#Sorting_hat data generation and compression
+python3 Sorting_hat/data_generation_sorting_hat.py Sorting_Hat/data/
+python3 utils/file_compression.py Sorting_Hat/data/ sortinghat_project_3* sortinghat_project_3.zip
