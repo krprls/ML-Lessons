30c3009eca18a2fa43f12805a36f2c26e54c06f8
AB <aarushban@gmail.com>
2019-07-11 12:02:06 -0700

    Added Averages commands to bash script file

--- update_all_files.sh
+++ update_all_files.sh
@@ -8,3 +8,7 @@ python3 utils/file_compression.py Guess_My_Age/data/ age_project_4* age_project_
 #Guess_My_Mood data generation and compression
 python3 Guess_My_Mood/data_generation_mood.py Guess_My_Mood/data/
 python3 utils/file_compression.py Guess_My_Mood/data/ mood_project_4* mood_project_4.zip
+
+#Averages data generation and compression
+python3 Averages/data_generation_averages.py Averages/data/
+python3 utils/file_compression.py Averages/data/ averages_project_4* averages_project_4.zip
