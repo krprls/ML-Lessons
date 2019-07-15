9032b5a2584aac0f5b9f62eb857085de3dc609e4
sindhuPyxeda <sindhu@pyxeda.ai>
2019-07-15 10:48:35 -0700

    Diabetes:Enable two ways to input features

--- Diabetes/projects/diabetes_project_1.py
+++ Diabetes/projects/diabetes_project_1.py
@@ -3,49 +3,127 @@ import requests
 import json as json
 import numpy as np
 
-def main():
+import argparse
+import tkinter as tk
+import PySimpleGUI as sg
 
-    print("Hello! Today we will use ML to see if a person has diabetes.")
+# Sample format of data expected
+# {"Pregnancies":6,"Glucose":148,"BloodPressure":72,"SkinThickness":35,
+# "Insulin":0,"BMI":33.6,"DiabetesPedigreeFunction":0.627,"Age":50}
 
-    url = input("What is your endpoint URL?\n")
-    url = url.strip()
+def get_multiple_predictions(values, fields):
+    """
+    This function calculates the predictions on
+    multiple samples
+    Args:
+    values (dict): Dictionary containing url and feature values
+    fields (list): List of strings containing feature names
+    Returns:
+    predictions (list): Contains the list of predictions
+    """
+    values_age = values['Age']
+    values_split = values_age.split(',')
 
-    #get user input 
-    pregnancies = input("How many pregnancies did this person experience?\n")
-    glucose = input("What is this person's glucose concentration?\n")
-    blood_pressure = input("What is this person's blood pressure (mm Hg)?\n")
-    skin_thickness = input("What is this person's skin fold thickness (mm)?\n")
-    insulin = input("What is this person's 2-hour serum insulin level (mu U/ml)?\n")
-    bmi = input("What is this person's BMI?\n")
-    diabetes_pedigree_function = input("What is this person's Diabetes Pedigree Function?\n")
-    age = input("What is this person's age?\n")
+    arg_list = []
 
-    #pass in the data
-    data = {"Pregnancies": pregnancies,"Glucose": glucose, "BloodPressure": blood_pressure, "SkinThickness": skin_thickness,
-            "Insulin": insulin, "BMI": bmi, "DiabetesPedigreeFunction": diabetes_pedigree_function, "Age": age}
-    
-    prediction = get_prediction(url, data)
-    
-    if float(prediction) == 1.0:
-        print("This person has diabetes.")
-    elif float(prediction) == 0.0:
-        print("This person does not have diabetes.")
+    for a in range(0,len(values_split)):
+        args = {'url': values['url']}
+        feature_num = 1
+        body = '{'
+        for feature in fields:
+            if(feature == 'Age'):
+                body = body + '"' + feature + '"' + ':' + str(values_split[a]) + ','
+            else:
+                body = body + '"' + feature + '"' + ':' + str(values[feature]) + ','
+            feature_num = feature_num+1
+        body = body[0:-1] + '}'
+        args['body'] = body
+        arg_list.append(args)
+    predictions = []
+    for queries in arg_list:
+        prediction = json.loads(get_prediction(queries['url'],
+                                               queries['body']))
+        pred_value = json.loads(prediction['body'])['predicted_label']
+        predictions.append(pred_value)
+    return predictions
 
-def get_prediction(url, data={"Pregnancies":6,"Glucose":148,"BloodPressure":72,"SkinThickness":35,
-                              "Insulin":0,"BMI":33.6,"DiabetesPedigreeFunction":0.627,"Age":50}):
-    r = requests.post(url, data=json.dumps(data))
-    response = getattr(r,'_content').decode("utf-8")
-    response = json.loads(response)
-    prediction_object = json.loads(response['body'])
-    label = ""    
-    if 'predicted_label' in prediction_object:
-        label = prediction_object['predicted_label']
 
-    print("ML prediction") 
-    print("\tLabel: ", label)
-    print("\tModel: ", prediction_object['Model'])
-    # print("\tMessage: ", prediction_object['Message']) #UNCOMMENT WHEN MESSAGE IS PART OF REGRESSION MODELS
-    return label
+def ui_mode():
+    """
+    This function generates a UI where the user can enter
+    feature values and see responses
+    """
+    # Create a dialog box with the features
+    fields = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+              'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+    layout = [[sg.Text('url'), sg.Input('', key='url')],
+              [sg.Text('Pregnancies'), sg.Input(6, key='Pregnancies')],
+              [sg.Text('Glucose'), sg.Input(148, key='Glucose')],
+              [sg.Text('BloodPressure'), sg.Input(72, key='BloodPressure')],
+              [sg.Text('SkinThickness'), sg.Input(35, key='SkinThickness')],
+              [sg.Text('Insulin'), sg.Input(0, key='Insulin')],
+              [sg.Text('BMI'), sg.Input(33.6, key='BMI')],
+              [sg.Text('DiabetesPedigreeFunction'), sg.Input(0.627, key='DiabetesPedigreeFunction')],
+              [sg.Text('Age', text_color='red'), sg.Input(50, key='Age')],
+              [sg.Text('Prediction: ', text_color='blue', size=(40, 2), key='_PRED_')],
+              [sg.RButton('Predict'), sg.Exit()]]
+    window = sg.Window('Enter url and feature values', layout)
+    while True:
+        event, values = window.Read()
+        predictions = str(get_multiple_predictions(values, fields))
+        predictions.replace('"', '')
+        if event == 'Predict':
+            print('entered predict event')
+            # change the "output" element to be the value of "input" element
+            window.Element('_PRED_').Update('Age: [' + values['Age'] + ']\n' + 
+                                            'Prediction: ' + predictions)
+        if event is None or event == 'Exit':
+            break
 
-if __name__ == "__main__":
-    main()
+    window.Close()
+
+
+def parseArguments():
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+
+    # Positional mandatory arguments
+    parser.add_argument("--body", help="text in the format provided by snippet in Navigator", type=str)
+    parser.add_argument("--url", help="Your endpoint URL", type=str)
+
+    # Parse arguments
+    try:
+        args = parser.parse_args()
+    except:
+        return None
+
+    return args
+
+
+def get_prediction(url, data='{"Pregnancies":6,'
+                             '"Glucose":148,'
+                             '"BloodPressure":72,'
+                             '"SkinThickness":35,'
+                             '"Insulin":0,'
+                             '"BMI":33.6,'
+                             '"DiabetesPedigreeFunction":0.627,'
+                             '"Age":50}'):
+    r = requests.post(url, data=data)
+    response = getattr(r, '_content').decode("utf-8")
+    return response
+
+if __name__ == '__main__':
+    # Parse the arguments
+    # Return None when no arguments are provided
+    args = parseArguments()
+    print('args: ', args)
+    # Print the arguments provided, when present
+    if args.url and args.body:
+        for a in args.__dict__:
+            print(str(a) + ": " + str(args.__dict__[a]))
+        print(get_prediction(args.url, args.body))
+    else:
+        # When no arguments are provided, pop up a
+        # dialog box where the user can enter 
+        # arguments
+        args_list = ui_mode()
