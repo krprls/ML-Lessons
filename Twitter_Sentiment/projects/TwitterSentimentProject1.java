c5de2f8d73d906e626af922578da326c1f8baf98
AB <aarushban@gmail.com>
2019-07-23 13:20:12 -0700

    Create TwitterSentimentProject1.java

---
+++ Twitter_Sentiment/projects/TwitterSentimentProject1.java
@@ -0,0 +1,122 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TwitterSentimentProject1 extends JFrame implements ActionListener {
+
+	private JPanel contentPane;
+	private String[] labelNames = { "Expression" };
+
+	private String[] defaultValues = {
+			"@DaRealSunisaKim Thanks for the Twitter add, Sunisa! I got to meet you once at a HIN show "
+					+ "here in the DC area and you were a sweetheart. " };
+
+	private Map<String, JLabel> labels = new HashMap<>();
+	private Map<String, JTextField> userInput = new HashMap<>();
+
+	// x and y coordinates of panel, width and height of panel
+	private int x, y, width, height;
+
+	private String data;
+	private JLabel prediction;
+	private JTextField endpointURL;
+
+	public TwitterSentimentProject1(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		data = "";
+		prediction = new JLabel("Prediction: ");
+		endpointURL = new JTextField();
+	}
+
+	public String makePrediction() {
+		URL url;
+		String pred = "";
+		try {
+			url = new URL(endpointURL.getText());
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setDoOutput(true);
+
+			// write string for request
+			OutputStream os = con.getOutputStream();
+			byte[] input = data.getBytes("utf-8");
+			os.write(input, 0, input.length);
+
+			// read and output resulting prediction
+			BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+			StringBuilder response = new StringBuilder();
+			String responseLine = null;
+			while ((responseLine = br.readLine()) != null) {
+				response.append(responseLine.trim());
+			}
+
+			// extracting prediction from response
+			int index = response.indexOf("\\\"predicted_label\\\": ") + "\\\"predicted_label\\\": ".length();
+			pred = response.substring(index);
+			pred = (pred.substring(0, pred.indexOf(","))).replaceAll("[^0-9]", "");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return pred;
+	}
+
+	private void generatePanelContent() {
+
+		// set panel configuration
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(this.x, this.y, this.width, this.height);
+		setTitle("Twitter Sentiment Prediction Service");
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(labelNames.length + 2, 2));
+
+		// added URL text field
+		contentPane.add(new JLabel("URL"));
+		contentPane.add(endpointURL);
+
+		// adding attribute text fields to panel
+		for (int i = 0; i < labelNames.length; i++) {
+			labels.put(labelNames[i], new JLabel(labelNames[i]));
+			userInput.put(labelNames[i] + "Input", new JTextField(defaultValues[i]));
+			contentPane.add(labels.get(labelNames[i]));
+			contentPane.add(userInput.get(labelNames[i] + "Input"));
+		}
+
+		JButton predictButton = new JButton("Predict");
+		predictButton.addActionListener(this);
+		contentPane.add(predictButton);
+		contentPane.add(prediction);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// getting data from text fields and making JSON
+		data = "";
+		data += "{";
+		for (int i = 0; i < labelNames.length; i++) {
+			data += "\"" + labelNames[i] + "\":\"" + userInput.get(labelNames[i] + "Input").getText() + "\"";
+			if (i != labelNames.length - 1) {
+				data += ",";
+			}
+		}
+		data += "}";
+
+		// gives user back prediction in GUI
+		prediction.setText("Prediction: " + makePrediction());
+	}
+
+	public static void main(String[] args) {
+		TwitterSentimentProject1 twitterSentiment = new TwitterSentimentProject1(0, 0, 800, 150);
+		twitterSentiment.generatePanelContent();
+		twitterSentiment.setVisible(true);
+	}
+
+}
